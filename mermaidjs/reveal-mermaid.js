303b2609ee8a4d591a44f1cafeefc6f4da8b0582
khaled agrama <khaled@users.noreply.github.com>
2022-05-18 09:54:23 -0400

    Closes #3

--- mermaidjs/reveal-mermaid.js
+++ mermaidjs/reveal-mermaid.js
@@ -9,7 +9,7 @@ Reveal.addEventListener('ready', event => asyncMermaidRender(event));
 async function asyncMermaidRender(event) {
     var graphs = document.getElementsByClassName("mermaid");
     graphs.forEach((item, index) => {
-        let graphCode = item.innerText.trim(); //trim() becase of gantt, class and git diagram
+        let graphCode = item.textContent.trim(); //trim() becase of gantt, class and git diagram
         let mermaidDiv = document.createElement('div');
         mermaidDiv.classList.add('mermaid');
         mermaidDiv.setAttribute("data-processed", "true");
